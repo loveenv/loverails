256220eb2948e5cebd6906f6ed424c668be014af
Loveen V <loveenv@gmail.com>
2013-10-06 09:22:20 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loverails::Application.config.secret_token = 'bc6207eb6fb4189cbd95eb680eb56fd65d1cfb40c94799f25d528034dde5c1a47efb992dac90e359f42b1b7290663ea856e6bd633cc96a3cf4785cd443f8abb5'
